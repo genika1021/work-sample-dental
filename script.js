5dcd258039589da1b9cd904c8a9e3c8b01ba0486
genika1021 <145639830+genika1021@users.noreply.github.com>
2023-09-29 12:15:40 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $("img#thumb-1").hover(function() {
+    $("#implants").show("slow");
+  });
+
+  $("img#thumb-2").hover(function() {
+    $("#family").show("slow");
+  });
+
+  $("img#thumb-3").hover(function() {
+    $("#child").show("slow");
+  });
+});
